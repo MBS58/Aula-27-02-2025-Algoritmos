930fcdaacf89ec2b776185368b17ea57f66c7189
MBS58 <matheusstein2019@outlook.com>
2025-04-24 15:10:23 -0300

    exer 4

---
+++ Aula24.04.2025Algoritmo/exer4.py
@@ -0,0 +1,13 @@
+# iniciando a variavel A
+a = 0 
+# iniciando a variavel X
+x = 0
+# while para x pular de 10 em 10 até 100 
+while (x < 100):
+    # soma de X 
+    x = x + 10
+    # acumulador A 
+    a = a + x
+    
+#printando o Valor de A
+print("Valor de A:", a)
